7d8f57ecd9a07c9366fabe7dcd64c104a681a213
Oussama Ezzaou <oussamaezzaou@gmail.com>
2025-03-02 12:09:34 +0000

    cub3d: formatting > include/

--- include/_types.h
+++ include/_types.h
@@ -6,7 +6,7 @@
 /*   By: hael-mou <hael-mou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 11:35:57 by hael-mou          #+#    #+#             */
-/*   Updated: 2025/02/28 10:24:37 by oezzaou          ###   ########.fr       */
+/*   Updated: 2023/11/28 10:24:37 by oezzaou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,111 +23,111 @@
 /* ************************************************************************** */
 /*                                   token                                    */
 /* ************************************************************************** */
-typedef struct s_token
+typedef struct  s_token
 {
-	char	*key;
-	char	*value;
-}	t_token;
+	char          *key;
+	char          *value;
+}               t_token;
 
 /* ************************************************************************** */
 /*                                   POINT                                    */
 /* ************************************************************************** */
-typedef struct s_point
+typedef struct  s_point
 {
-	int32_t			x;
-	int32_t			y;
-}	t_point;
+	int32_t			  x;
+	int32_t			  y;
+}               t_point;
 
 /* ************************************************************************** */
 /*                                    RAY                                     */
 /* ************************************************************************** */
-typedef struct s_ray
+typedef struct  s_ray
 {
-	t_point			pos_map;
-	t_vect2d		direction;
-	t_point			step;
-	t_vect2d		delta;
-	t_vect2d		s_dist;
-}	t_ray;
+	t_point			  pos_map;
+	t_vect2d		  direction;
+	t_point			  step;
+	t_vect2d		  delta;
+	t_vect2d		  s_dist;
+}	              t_ray;
 
-typedef struct s_final_ray
+typedef struct  s_final_ray
 {
-	t_vect2d		origin;
-	t_vect2d		direction;
-	int32_t			side;
-	bool			is_door;
-	double			distance;
-}	t_final_ray;
+	t_vect2d		  origin;
+	t_vect2d		  direction;
+	int32_t			  side;
+	bool			    is_door;
+	double			  distance;
+}               t_final_ray;
 
 
 //====<[ Struct: wall ]>========================================================
-typedef struct s_wall
+typedef struct  s_wall
 {
-	int				height;
-	int				y_min;
-	int				y_moy;
-	int				y_max;
-	float			intensity;
+	int				    height;
+	int				    y_min;
+	int				    y_moy;
+	int				    y_max;
+	float			    intensity;
 	mlx_texture_t	*txtr;
-	double			c;
-}					t_wall;
+	double			  c;
+}					      t_wall;
 
 /* ************************************************************************** */
 /*                                  CAMERA                                    */
 /* ************************************************************************** */
-typedef struct s_camera
+typedef struct  s_camera
 {
-	t_vect2d		position;
-	t_vect2d		direction;
-	t_vect2d		plane;
-}	t_camera;
+	t_vect2d		  position;
+	t_vect2d		  direction;
+	t_vect2d		  plane;
+}               t_camera;
 
 /* ************************************************************************** */
 /*                                   ACTION                                   */
 /* ************************************************************************** */
-typedef struct s_action
+typedef struct  s_action
 {
-	mlx_texture_t	*sprite;
-	uint32_t		frame;
-	uint32_t		frame_size;
-	char			*sound;
-}	t_action;
+	mlx_texture_t *sprite;
+	uint32_t		  frame;
+	uint32_t		  frame_size;
+	char			    *sound;
+}               t_action;
 
 /* ************************************************************************** */
 /*                                   PLAYER                                   */
 /* ************************************************************************** */
-typedef struct s_player
+typedef struct  s_player
 {
 	mlx_image_t		*img;
-	int16_t			mode;
-	t_action		action[MAX_ACTION];
-	int16_t			active_action;
-}	t_player;
+	int16_t			  mode;
+	t_action		  action[MAX_ACTION];
+	int16_t			  active_action;
+}               t_player;
 
 //===<[ Struct : Date ]>========================================================
-typedef struct s_data
+typedef struct  s_data
 {
-	char			**map;
-	int32_t			height;
-	uint32_t		ceiling;
-	uint32_t		floor;
+	char			    **map;
+	int32_t			  height;
+	uint32_t		  ceiling;
+	uint32_t		  floor;
 	mlx_texture_t	*door;
 	mlx_texture_t	*wall[4];
-	int32_t			prespective;
-}	t_data;
+	int32_t			  prespective;
+}	              t_data;
 
 /* ************************************************************************** */
 /*                                   ENGINE                                   */
 /* ************************************************************************** */
-typedef struct s_engine
+typedef struct  s_engine
 {
-	mlx_t			*mlx;
-	t_data			*data;
-	t_camera		*cam;
+	mlx_t			    *mlx;
+	t_data			  *data;
+	t_camera		  *cam;
 	mlx_image_t		*view;
 	mlx_image_t		*mini;
-	t_player		player;
-}	t_engine;
+	t_player		  player;
+}	              t_engine;
 
 #endif /*___TYPES_H__*/
 //==============================================================================
--- include/cub3d.h
+++ include/cub3d.h
@@ -5,7 +5,7 @@
 //  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀                                              
 //  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇                                              
 //  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2023/10/11 08:45:42 by oezzaou
-//  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/02/28 10:28:03 by oezzaou
+//  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/03/02 12:08:56 by oezzaou
 //  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀                                              
 //  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀                                              
 //  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪      
@@ -26,40 +26,40 @@
 /* ************************************************************************** */
 /*                                  CORE                                      */
 /* ************************************************************************** */
-void		engine_clean(t_engine *inst, int exit_status);
-void		engine_init(t_engine *inst);
-void		engine_start(t_engine *inst);
-void		key_handler(t_engine *inst);
-void		rotate_camera(t_camera *cam, double angle);
-void		move_camera(t_camera *cam, double mv_step, t_data *d, int32_t move);
-void		mouse_key_han(mouse_key_t b, action_t a, modifier_key_t m, void *p);
-void		mouse_handler(t_engine *inst);
-void		animator(t_engine *engine);
-void		shoot(t_engine *inst);
-void		aim(t_engine *inst);
+void		    engine_clean(t_engine *inst, int exit_status);
+void		    engine_init(t_engine *inst);
+void		    engine_start(t_engine *inst);
+void		    key_handler(t_engine *inst);
+void		    rotate_camera(t_camera *cam, double angle);
+void		    move_camera(t_camera *cam, double mv_step, t_data *d, int32_t move);
+void		    mouse_key_han(mouse_key_t b, action_t a, modifier_key_t m, void *p);
+void		    mouse_handler(t_engine *inst);
+void		    animator(t_engine *engine);
+void		    shoot(t_engine *inst);
+void		    aim(t_engine *inst);
 
 //====<[ Functions:  Loader ]>==================================================
-t_data		*loader(char const *file);
-bool		load_player(t_player *player);
-bool		load_assets(char *line, t_data *data);
-t_camera	*init_camera(char **map);
-void		set_camera(t_camera *cam, int x, int y, char *dir);
-char		*get_line(int fd, int index);
-t_token		tokenizer(char *line);
-bool		check_unit(char **map, int row, int colum);
-void		*clean_data(t_data *data);
+t_data		  *loader(char const *file);
+bool		    load_player(t_player *player);
+bool		    load_assets(char *line, t_data *data);
+t_camera	  *init_camera(char **map);
+void		    set_camera(t_camera *cam, int x, int y, char *dir);
+char		    *get_line(int fd, int index);
+t_token		  tokenizer(char *line);
+bool		    check_unit(char **map, int row, int colum);
+void		    *clean_data(t_data *data);
 
 
 //====<[ Functions: Raycaster ]>================================================
 t_final_ray	raycaster(t_vect2d start, t_vect2d ray_dir, t_data *data);
-void		maping_textures(mlx_image_t *v, int x, t_final_ray *r, t_data *d);
-uint32_t	change_pixel_intensity(uint32_t pixel, float intensity);
+void		    maping_textures(mlx_image_t *v, int x, t_final_ray *r, t_data *d);
+uint32_t	  change_pixel_intensity(uint32_t pixel, float intensity);
 
 /* ************************************************************************** */
 /*                                SCENES                                      */
 /* ************************************************************************** */
-void		minimap(mlx_image_t *img, t_data *data, t_vect2d cam);
-void		render_view(mlx_image_t *view, t_camera *cam, t_data *data);
+void		    minimap(mlx_image_t *img, t_data *data, t_vect2d cam);
+void		    render_view(mlx_image_t *view, t_camera *cam, t_data *data);
 
 #endif /*__CUB3D_H__*/
 //==============================================================================
